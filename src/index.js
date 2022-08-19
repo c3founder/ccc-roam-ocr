84d041fa568fcfa784201ad75bd2763bee7f637b
Josh <brownjoshua490@gmail.com>
2022-08-19 15:33:03 -0600

    fix util def

--- src/index.js
+++ src/index.js
@@ -103,26 +103,26 @@ const panelConfig = {
 
 let ccc = {};
 
-ccc.util = {
+ccc.util = ((c3u) => {
     ///////////////Front-End///////////////
-    getUidOfContainingBlock: (el) => {
+    c3u.getUidOfContainingBlock = (el) => {
         return el.closest('.rm-block__input').id.slice(-9)
-    },
+    }
 
-    insertAfter: (newEl, anchor) => {
+    c3u.insertAfter = (newEl, anchor) => {
         anchor.parentElement.insertBefore(newEl, anchor.nextSibling)
-    },
+    }
 
-    getNthChildUid: (parentUid, order) => {
+    c3u.getNthChildUid = (parentUid, order) => {
         const allChildren = c3u.allChildrenInfo(parentUid)[0][0].children;
         const childrenOrder = allChildren.map(function (child) { return child.order; });
         const index = childrenOrder.findIndex(el => el === order);
         return index !== -1 ? allChildren[index].uid : null;
-    },
+    }
 
-    sleep: m => new Promise(r => setTimeout(r, m)),
+    c3u.sleep = m => new Promise(r => setTimeout(r, m))
 
-    createPage: (pageTitle) => {
+    c3u.createPage = (pageTitle) => {
         let pageUid = c3u.createUid()
         const status = window.roamAlphaAPI.createPage(
             {
@@ -130,15 +130,15 @@ ccc.util = {
                 { "title": pageTitle, "uid": pageUid }
             })
         return status ? pageUid : null
-    },
+    }
 
-    updateBlockString: (blockUid, newString) => {
+    c3u.updateBlockString = (blockUid, newString) => {
         return window.roamAlphaAPI.updateBlock({
             block: { uid: blockUid, string: newString }
         });
-    },
+    }
 
-    hashCode: (str) => {
+    c3u.hashCode = (str) => {
         let hash = 0, i, chr;
         for (i = 0; i < str.length; i++) {
             chr = str.charCodeAt(i);
@@ -146,60 +146,60 @@ ccc.util = {
             hash |= 0; // Convert to 32bit integer
         }
         return hash;
-    },
+    }
 
-    createChildBlock: (parentUid, order, childString, childUid) => {
+    c3u.createChildBlock = (parentUid, order, childString, childUid) => {
         return window.roamAlphaAPI.createBlock(
             {
                 location: { "parent-uid": parentUid, order: order },
                 block: { string: childString.toString(), uid: childUid }
             })
-    },
+    }
 
-    openBlockInSidebar: (windowType, blockUid) => {
+    c3u.openBlockInSidebar = (windowType, blockUid) => {
         return window.roamAlphaAPI.ui.rightSidebar.addWindow({ window: { type: windowType, 'block-uid': blockUid } })
-    },
+    }
 
-    deletePage: (pageUid) => {
+    c3u.deletePage = (pageUid) => {
         return window.roamAlphaAPI.deletePage({ page: { uid: pageUid } });
-    },
+    }
 
 
-    createUid: () => {
+    c3u.createUid = () => {
         return roamAlphaAPI.util.generateUID();
-    },
+    }
 
 
 
     ///////////////Back-End///////////////
-    existBlockUid: (blockUid) => {
+    c3u.existBlockUid = (blockUid) => {
         const res = window.roamAlphaAPI.q(
             `[:find (pull ?block [:block/uid])
         :where
                [?block :block/uid \"${blockUid}\"]]`)
         return res.length ? blockUid : null
-    },
+    }
 
-    deleteBlock: (blockUid) => {
+    c3u.deleteBlock = (blockUid) => {
         return window.roamAlphaAPI.deleteBlock({ "block": { "uid": blockUid } });
-    },
+    }
 
-    parentBlockUid: (blockUid) => {
+    c3u.parentBlockUid = (blockUid) => {
         const res = window.roamAlphaAPI.q(
             `[:find (pull ?parent [:block/uid])
         :where
             [?parent :block/children ?block]
                [?block :block/uid \"${blockUid}\"]]`)
         return res.length ? res[0][0].uid : null
-    },
+    }
 
-    blockString: (blockUid) => {
+    c3u.blockString = (blockUid) => {
         return window.roamAlphaAPI.q(
             `[:find (pull ?block [:block/string])
         :where [?block :block/uid \"${blockUid}\"]]`)[0][0].string
-    },
+    }
 
-    allChildrenInfo: (blockUid) => {
+    c3u.allChildrenInfo = (blockUid) => {
         let results = window.roamAlphaAPI.q(
             `[:find (pull ?parent 
                 [* {:block/children [:block/string :block/uid :block/order]}])
@@ -207,25 +207,25 @@ ccc.util = {
           [?parent :block/uid \"${blockUid}\"]]`)
         return (results.length == 0) ? undefined : results
 
-    },
+    }
 
-    queryAllTxtInChildren: (blockUid) => {
+    c3u.queryAllTxtInChildren = (blockUid) => {
         return window.roamAlphaAPI.q(`[
             :find (pull ?block [
                 :block/string
                 {:block/children ...}
             ])
             :where [?block :block/uid \"${blockUid}\"]]`)
-    },
+    }
 
-    getPageUid: (pageTitle) => {
+    c3u.getPageUid = (pageTitle) => {
         const res = window.roamAlphaAPI.q(
             `[:find (pull ?page [:block/uid])
         :where [?page :node/title \"${pageTitle}\"]]`)
         return res.length ? res[0][0].uid : null
-    },
+    }
 
-    getOrCreatePageUid: (pageTitle, initString = null) => {
+    c3u.getOrCreatePageUid = (pageTitle, initString = null) => {
         let pageUid = c3u.getPageUid(pageTitle)
         if (!pageUid) {
             pageUid = c3u.createPage(pageTitle);
@@ -233,9 +233,9 @@ ccc.util = {
                 c3u.createChildBlock(pageUid, 0, initString, c3u.createUid());
         }
         return pageUid;
-    },
+    }
 
-    isAncestor: (a, b) => {
+    c3u.isAncestor = (a, b) => {
         const results = window.roamAlphaAPI.q(
             `[:find (pull ?root [* {:block/children [:block/uid {:block/children ...}]}])
             :where
@@ -244,14 +244,16 @@ ccc.util = {
         let descendantUids = [];
         c3u.getUidFromNestedNodes(results[0][0], descendantUids)
         return descendantUids.includes(b);
-    },
+    }
 
-    getUidFromNestedNodes: (node, descendantUids) => {
+    c3u.getUidFromNestedNodes = (node, descendantUids) => {
         if (node.uid) descendantUids.push(node.uid)
         if (node.children)
             node.children.forEach(child => c3u.getUidFromNestedNodes(child, descendantUids))
     }
-};
+
+    return c3u;
+})(ccc.util || {});
 
 /* Begin Importing Utility Functions */
 function onload({ extensionAPI }) {
